0c306322c55e1a37033389fc991ea11839932970
modisatyam <satyammodi1415@gmail.com>
2024-06-29 16:28:54 +0530

    Add files via upload

---
+++ partials/_db_connect.php
@@ -0,0 +1,10 @@
+<?php
+
+$_SERVER = "localhost";
+$dbname = "furniflex";
+$password="";
+$user="root";
+
+$conn = mysqli_connect($_SERVER,$user,$password,$dbname);
+
+?>
---
+++ partials/_navbar.php
@@ -0,0 +1,26 @@
+
+<nav>
+    
+<div class="left_nav">
+  
+  <ul>
+    <h1>Furniflex</h1>
+    <li><a href="index.php">Home</a></li>
+    <li><a href="Furniture.php">Furniture</a></li>
+    <li><a href="About.php">About</a></li>
+  </ul>
+</div>
+<div class="right_nav">
+  <ul>
+
+    <button onclick="window.location='signin.php'">Login</button>
+    <button onclick="window.location='registration.php'">Sign Up</button>
+<li>
+    <img src="images/profile.png" alt="profile" width="30px"></li>
+  </ul>
+</div>
+</nav>
+
+
+
+  
